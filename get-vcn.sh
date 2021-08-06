1104ae86ba5c7b72f6a6dc6fba250cd056ca7c89
ufierro <ufierroruvalcaba@gmail.com>
2021-08-06 14:03:50 -0600

    included version override setting for action

--- get-vcn.sh
+++ get-vcn.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-version_string=$(curl -s https://api.github.com/repos/codenotary/vcn/releases/latest | awk '/tag_name/ {gsub(/"/,""); gsub(/,/,""); print $2}')
+if [[ $# -gt  0 ]]; then
+    echo "Obtained version input: $1 from workflow; The action will use this instead of looking \
+     for the latest available release. If this is a mistake, please remove the 'version' section from this action\
+     and restart your workflow to use latest available release"
+    version_string=$1
+fi
+
+if [[ -z "${version_string}" ]]; then
+    version_string=$(curl -s https://api.github.com/repos/codenotary/vcn/releases/latest | awk '/tag_name/ {gsub(/"/,""); gsub(/,/,""); print $2}')
+    echo "Latest available release for VCN is: $version_string"
+fi
 base_url=https://github.com/codenotary/vcn/releases/latest/download
 linux_file="$base_url/vcn-$version_string-linux-amd64-static"
 windows_file="$base_url/vcn-$version_string-windows-amd64.exe"
@@ -17,3 +27,4 @@ else
 fi
 
 curl -s -L "$target" -o vcn"$out" && chmod +x vcn*
+echo "Succesfully acquired VCN binary"
